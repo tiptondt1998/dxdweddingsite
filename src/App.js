75ee7c07520aef09bc69906fa383fcab4bf952fb
tiptondt1998 <dylan.tipton@live.com>
2021-11-09 00:10:24 -0500

    rolled back changes

--- src/App.js
+++ src/App.js
@@ -5,26 +5,13 @@ import RsvpForm from './RsvpForm/index'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import {useState} from 'react';
-
-const Page = ({name}) => {
-  switch(name){
-    case 'Venue': return <Venue />;
-  }
-}
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('Venue');
-  const handlePageChange = (value) => setCurrentPage(value);
 
   return(
     <div>
-          <div id="nav">
-      <Nav handlePageChange ={handlePageChange} />
-      </div>
-      <div>
-        <Page name={currentPage} />
-      </div> 
+      <Venue/>
+      <RsvpForm/>
     </div>
   );
 }
