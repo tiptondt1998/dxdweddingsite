f6c25e9f3a43951f2883ee2c5808317c32819ceb
tiptondt1998 <dylan.tipton@live.com>
2021-11-08 22:12:41 -0500

    edited app file

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ import RsvpForm from './RsvpForm/index'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem('id_token');
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    });
-  },
-  uri: 'http://localhost:3001/graphql',
-});
+// const client = new ApolloClient({
+//   request: (operation) => {
+//     const token = localStorage.getItem('id_token');
+//     operation.setContext({
+//       headers: {
+//         authorization: token ? `Bearer ${token}` : '',
+//       },
+//     });
+//   },
+//   uri: 'http://localhost:3001/graphql',
+// });
 
 function App() {
   return(
-<ApolloProvider client={client}>
+    <div>
+    The wedding will take place at Margartiaville Beach Hotel.
+    <br/>
+    Address: 165 FORT PICKENS ROAD, PENSACOLA BEACH, FL 32561
+    <Footer/>
+    </div>
+/* <ApolloProvider client={client}>
   <Router>
         <div className="App">
       <Route exact path='/' component={Venue}/>
@@ -29,7 +35,7 @@ function App() {
     </div>
   );
   </Router>
-</ApolloProvider>
+</ApolloProvider> */
   );
 }
 
