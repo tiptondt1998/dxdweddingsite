84beb2df969c2881276deb14a9e5346193207ab5
tiptondt1998 <dylan.tipton@live.com>
2021-11-08 20:53:39 -0500

    added apollo router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import Venue from './Venue/index';
 import RsvpForm from './RsvpForm/index'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+  uri: 'http://localhost:3001/graphql',
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Venue />
-      </header>
-      <footer>
+  return(
+<ApolloProvider client={client}>
+  <Router>
+        <div className="App">
+      <Route exact path='/' component={Venue}/>
+      {/* <footer>
         <RsvpForm />
-      </footer>
+      </footer> */}
     </div>
+  );
+  </Router>
+</ApolloProvider>
   );
 }
 
